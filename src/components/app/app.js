e5d6810aabe2f602f49ff34b06b22443a79b7e8b
Alexandr <ak4raulov@yandex.ru>
2021-12-15 13:40:20 +0300

    практика свойств на проекте

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,15 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
 function App() {
+	//Имитация базы данных
+	const data = [
+		{ name: 'Alex K.', salary: 1500, increase: true },
+		{ name: 'Ivan P.', salary: 700, increase: false },
+		{ name: 'Inna R.', salary: 1700, increase: true },
+		{ name: 'Roza P.', salary: 890, increase: false },
+		{ name: 'Sergy V.', salary: 900, increase: false },
+		{ name: 'Maks R.', salary: 1100, increase: true }
+	];
 	return (
 		<div className="app">
 			<AppInfo />
@@ -13,10 +22,9 @@ function App() {
 				<SearchPanel />
 				<AppFilter />
 			</div>
-			<EmployersList />
+			<EmployersList data={data}/>
 			<EmployersAddForm />
 		</div>
 	);
 }
-
 export default App;
